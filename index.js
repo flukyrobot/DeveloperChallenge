abb888a18d38cf1340c383ce5817b78bca3537fe
FlukyRobot <paul@flukyrobot.com>
2022-04-08 15:26:29 +0000

    wip

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
-const PREAMBLE = 'CAPTIVATION';
+const PREAMBLE = "CAPTIVATION";
 const PREAMBLE_BINARY = toBinary(PREAMBLE);
-const MESSAGE_LENGTH = PREAMBLE_BINARY + 8 * 100;
+const MESSAGE_LENGTH = PREAMBLE_BINARY.length + 8 * 100;
 
-console.log('preamble binary', PREAMBLE_BINARY)
+let messageBuffer = "";
+let outputCount = 0;
+const messageBufferArray = [];
+let chunkCounter = 0;
 
-let messageBuffer = '';
-let failureBuffer = '';
-let maxCorrect = ''
-let maxLen;
-let index = -1;
-
-let all = ''
 function main() {
-console.log('looking for', PREAMBLE_BINARY)
-
   // process.stdin.on('data', test);
-  // process.stdin.on('end', () => console.log(failureBuffer))
-  process.stdin.on('data', handleChunk);
-  process.stdin.on('error', handleError);
+  process.stdin.on("end", () => reportMetrics());
+  // process.stdin.on("data", handleChunkString);
+  process.stdin.on("data", handleChunkStringNaive);
+  // process.stdin.on("data", handleChunkArray);
+  process.stdin.on("error", handleError);
 }
 
 // function handleChunk(chunk) {
@@ -26,48 +22,116 @@ console.log('looking for', PREAMBLE_BINARY)
 // }
 
 function test(chunk) {
-  all += chunk.toString()
+  all += chunk.toString();
 }
 
-function handleChunk(chunk) {
-  const parts = chunk.toString().split('');
-  for(const char of parts) {
-    index++;
-    // console.log('adding char', char, messageBuffer.length)
-      messageBuffer += char;
-      failureBuffer += char;
-    if(messageBuffer.length <= PREAMBLE_BINARY.length) {
-      while(messageBuffer.length && !PREAMBLE_BINARY.startsWith(messageBuffer)) {
-        console.log('\nresetting', messageBuffer, char, index)
-        messageBuffer = messageBuffer.slice(1)
-        console.log('aftereset', messageBuffer)
-        // console.log('failureBuffer', failureBuffer)
-        // console.log('resetting', messageBuffer, char, 'isnt', PREAMBLE_BINARY, messageBuffer.length, PREAMBLE_BINARY.length)
-        // resetBuffer();
+function handleChunkArray(chunk) {
+  messageBufferArray.push(...chunk.toString().split(""));
+  if (messageBufferArray.length >= MESSAGE_LENGTH) {
+    // console.log(test.length, messageBufferArray.length)
+
+    while (messageBufferArray.length >= MESSAGE_LENGTH) {
+      const test = messageBufferArray.slice(0, PREAMBLE_BINARY.length).join("");
+      // console.log("message", message, PREAMBLE_BINARY);
+      if (!PREAMBLE_BINARY.startsWith(test)) {
+        messageBufferArray.splice(0, 1);
+      } else {
+        messageBufferArray.splice(0, PREAMBLE_BINARY.length);
+        const message = messageBufferArray.slice(0, 800).join("");
+        // console.log(decodeBinaryMessage(message));
       }
-    } else if(messageBuffer.length === MESSAGE_LENGTH) {
-      console.log('foudn message', messageBuffer)
-      console.log(messageBuffer.slice(PREAMBLE_BINARY.length))
+    }
+  }
+}
+
+let currentIndex = 0,
+  characters = [];
+function handleChunkString(chunk) {
+  messageBuffer += chunk.toString();
+  const preambleIndex = messageBuffer.indexOf(PREAMBLE_BINARY);
+  currentIndex = preambleIndex + PREAMBLE_BINARY.length - 1;
+  while (currentIndex + 8 < messageBuffer.length) {
+    console.log(currentIndex, characters.length);
+    if (
+      characters.length < 100 &&
+      (preambleIndex > -1 || characters.length > 0)
+    ) {
+      characters.push(messageBuffer.substring(currentIndex, currentIndex + 8));
+      currentIndex += 8;
+    }
+    if (characters.length === 100) {
+      console.log("covnerting mesg");
+      characters.forEach((character) =>
+        process.stdout.write(decodeBinaryMessage(character))
+      );
+      characters.length = 0;
+      messageBuffer = messageBuffer.slice(currentIndex);
+      currentIndex = 0;
+    }
+  }
+  console.log(characters.length);
+}
+
+function handleChunkStringNaive(chunk) {
+  chunkCounter++;
+  const parts = chunk.toString().split("");
+  for (const char of parts) {
+    // console.log('adding char', char, messageBuffer.length)
+    messageBuffer += char;
+    if (messageBuffer.length <= PREAMBLE_BINARY.length) {
+      while (
+        messageBuffer.length &&
+        !PREAMBLE_BINARY.startsWith(messageBuffer)
+      ) {
+        messageBuffer = messageBuffer.slice(1);
+      }
+    } else if (messageBuffer.length === MESSAGE_LENGTH) {
+      // console.log('foudn message', messageBuffer)
+      // console.log('parts', messageBuffer.split(/\d{8}/))
+      // console.log('ascii', String.fromCharCode(parseInt(messageBuffer, 2)))
+      console.log(
+        "message",
+        decodeBinaryMessage(messageBuffer.slice(PREAMBLE_BINARY.length))
+      );
       resetBuffer();
-      process.exit()
-    } 
-  }  
+      // process.exit()
+    }
+  }
+}
+
+function decodeBinaryMessage(binaryMessage) {
+  // console.log("binary message", binaryMessage);
+  let message = "";
+  for (let idx = 0; idx < binaryMessage.length; idx += 8) {
+    message += String.fromCharCode(
+      parseInt(binaryMessage.substring(idx, idx + 8), 2)
+    );
+  }
+  return message;
 }
 
 function resetBuffer() {
-  messageBuffer = '';
+  messageBuffer = "";
 }
 
 function toBinary(input) {
-  return input.split('').map(e => e.charCodeAt(0).toString(2).padStart(8, '0')).join('');
+  return input
+    .split("")
+    .map((e) => e.charCodeAt(0).toString(2).padStart(8, "0"))
+    .join("");
 }
 
 function handleError(err) {
   process.exit();
 }
 
-if(!module.parent) {
-  main();
-} 
+function reportMetrics() {
+  const used = process.memoryUsage().heapUsed / 1024 / 1024;
+  console.log(`Split uses approximately ${Math.round(used * 100) / 100} MB`);
+}
 
-module.exports = { PREAMBLE, PREAMBLE_BINARY, MESSAGE_LENGTH, toBinary }
+if (!module.parent) {
+  main();
+}
+
+module.exports = { PREAMBLE, PREAMBLE_BINARY, MESSAGE_LENGTH, toBinary };
--- test.js
+++ test.js
@@ -1,11 +1,16 @@
-const fs = require('fs');
-const {PREAMBLE_BINARY, MESSAGE_LENGTH, toBinary} = require('./');
-const toFind = 'GOUDAPEPPERJACKPARMASEANAMERICANGOUDAPEPPERJACKPARMASEANAMERICANGOUDAPEPPERJACKPARMASEANAMERICANXXXX';
+const fs = require("fs");
+const { PREAMBLE_BINARY, MESSAGE_LENGTH, toBinary } = require("./");
+const toFind =
+  "GOUDAPEPPERJACKPARMASEANAMERICANGOUDAPEPPERJACKPARMASEANAMERICANGOUDAPEPPERJACKPARMASEANAMERICANXXXX";
 const toFindCropped = toFind.substring(0, 100);
-console.log(toFind)
-console.log(toFindCropped)
+console.log(toFind);
+console.log(toFindCropped);
 const chars = toBinary(toFindCropped);
-console.log('chars', chars)
-const fullData = ('0101000101010' + PREAMBLE_BINARY + chars);
-console.log('fullData', fullData)
-fs.writeFileSync('test_data.txt', fullData , 'utf-8');
+console.log("chars", chars);
+const fullData = "0101000101010" + PREAMBLE_BINARY + chars;
+let fileData = "";
+// console.log('fullData', fullData)
+for (let count = 0; count < 1; count++) {
+  fileData += fullData;
+}
+fs.writeFileSync("test_data.txt", fileData, "utf-8");
